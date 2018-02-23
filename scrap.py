3aaae7d5322a82b6d244579aa71d48beb4f06ff9
Dmitry <dskurbakov@gmail.com>
2018-02-23 13:36:42 +0300

    For each website added description

--- scrap.py
+++ scrap.py
@@ -27,14 +27,23 @@ rows = list(soup.select("#container > div > div > div > div.results > div.challe
 
 titles = []
 locations = []
+preview = []
 descriptions = []
 time = []
 
 for row in rows:
     temp_title = row.find("h2", {"class": "title"})
     temp_location = row.find("p", {"class": "challenge-location"})
-    temp_description = row.find("p", {"class": "challenge-description"})
+    temp_preview = row.find("p", {"class": "challenge-description"})
     temp_time = row.find("span", {"class": "value date-range"})
+
+    temp_ref = row.find("a", href=True)['href'].encode('ascii', 'ignore')
+    driver.get(temp_ref)
+
+    temp_page = driver.page_source
+    temp_soup = BeautifulSoup(temp_page, 'html.parser')
+
+    temp_description = temp_soup.find("article", {"id": "challenge-description"})
 
     if hasattr(temp_title, "text"):
         titles.append(temp_title.text.strip())
@@ -46,8 +55,13 @@ for row in rows:
     else:
         locations.append("")
 
+    if hasattr(temp_preview, "text"):
+        preview.append(temp_preview.text.strip())
+    else:
+        preview.append("")
+
     if hasattr(temp_description, "text"):
-        descriptions.append(temp_description.text.strip())
+        descriptions.append(temp_description.text.replace("\n", "").replace('"', '').strip())
     else:
         descriptions.append("")
 
@@ -56,14 +70,15 @@ for row in rows:
     else:
         time.append("")
 
-file = open("data.json", "w")
+file = open("data1.json", "w")
 file.write('{\n\t"items":[\n')
 i = 0
 while 1:
     file.write('\t\t{\n')
     file.write('\t\t\t"title":' + '"' + titles[i].encode('ascii', 'ignore') + '"' + ',\n')
     file.write('\t\t\t"location":' + '"' + locations[i].encode('ascii', 'ignore') + '"' + ',\n')
-    file.write('\t\t\t"description":' + '"' + descriptions[i].encode('ascii', 'ignore') + '"' + ',\n')
+    file.write('\t\t\t"preview":' + '"' + preview[i].encode('ascii', 'ignore') + '"' + ',\n')
+    file.write('\t\t\t"description:":' + '"' + descriptions[i].encode('ascii', 'ignore') + '"' ',\n')
     file.write('\t\t\t"time":' + '"' + time[i].encode('ascii', 'ignore') + '"\n')
     if i + 1 < len(rows):
         file.write("\t\t},\n")
